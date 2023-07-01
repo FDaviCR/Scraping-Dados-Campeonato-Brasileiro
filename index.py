310f589c8d8b3718ce4040ef9fd3338bd13af5af
Davi <davicamiloribeiro@gmail.com>
2023-07-01 20:08:33 -0300

    Função para verificar se o campeonato ja foi cadastrado

--- index.py
+++ index.py
@@ -3,8 +3,22 @@ from Ws_partidas import getPartida
 from Ws_times import getTimes
 from Ws_campeonato import getCampeonato
 
-def cadastrarCampeonatoBrasileiroA(anoCampeonato):
+def verificarCampeonato(Campeonato, Divisao, Ano):
+    sql = ("select id, Campeonato, Divisao, Ano from campeonatos");
+    Connection.execute(sql);
+    campeonatosCadastradosRaw = Connection.fetchall();
+    
+    for item in campeonatosCadastradosRaw:  
+        print(item[1].lower() +'='+ Campeonato.lower() +'---'+ item[2].lower() +'='+ Divisao.lower() +'---'+ str(item[3]) +'='+ str(Ano))  
+        if(item[1].lower() == Campeonato.lower() and item[2].lower() == Divisao.lower() and str(item[3]) == str(Ano)):
+            return [True, item[0]]
+    return [False, 0]   
+
+def cadastrarCampeonato(anoCampeonato):
     dados = getCampeonato(anoCampeonato);
+    existeCampeonato = verificarCampeonato(dados[0], dados[1], dados[2])
+    print(dados[0]+" - "+dados[1]+" - "+dados[2]);
+    '''
     sqlCheck = ("select Campeonato, Divisao, Ano from campeonatos");
     Connection.execute(sqlCheck);
     campeonatosCadastradosRaw = Connection.fetchall();
@@ -12,8 +26,12 @@ def cadastrarCampeonatoBrasileiroA(anoCampeonato):
     
     for campeonato in campeonatosCadastradosRaw:
         campeonatosCadastrados.append(campeonato[0].lower()+campeonato[1].lower()+str(campeonato[2]));
-        
+      
     if((dados[0].lower()+dados[1].lower()+str(dados[2])) in campeonatosCadastrados):
+        print(dados[0]+" - "+dados[1]+" - "+dados[2] + " já cadastrado!");
+    '''  
+    print(existeCampeonato)
+    if(existeCampeonato[0]):
         print(dados[0]+" - "+dados[1]+" - "+dados[2] + " já cadastrado!");
     else:
         sql = ("INSERT INTO campeonatos (campeonato, divisao, ano, ativo) VALUES (%s, %s, %s, %s)");
@@ -36,14 +54,17 @@ def cadastrarTimes(anoCampeonato):
         if(time.lower() in timesCadastrados):
             print(time + " já cadastrado!")
         else:
+            apelidoRaw = time.split(' - ');
+            apelido = apelidoRaw[0].replace(' Saf','').replace(' S.a.f','')
             sql = ("INSERT INTO times (time, ativo, timeApelido) VALUES (%s, %s, %s)");
-            values = (time, 1, time);
+            values = (time, 1, apelido);
 
             Connection.execute(sql, values)
             executeDatabaseCommand();
         
 #cadastrarTimes(2023)     
-cadastrarCampeonatoBrasileiroA(2023)
+cadastrarCampeonato(2021)
+#print(verificarCampeonato('Campeonato Brasileiro de Futebol', 'Série A', 2021))
         
 '''
 if(hasattr(test, 'mandante_placar')):
