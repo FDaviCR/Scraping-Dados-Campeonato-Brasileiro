515804af717343af15510e3c7f101c5d6aa1e1e7
Davi <davicamiloribeiro@gmail.com>
2023-07-26 16:50:45 -0300

    Cadastro de Campeonato

--- index.py
+++ index.py
@@ -7,12 +7,19 @@ from Functions.index import cadastrarCampeonato
 anos = [2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023]
 
 def openFormCampeonatos():
-    frameForm.pack(padx=20, pady=40)
+    app.title('Cadastro de Campeonato Brasileiro')
+    frameFormCampeonato.pack(padx=20, pady=40)
     frameOptions.pack_forget()
     
 def closeFormCampeonatos():
-    frameForm.pack_forget()
+    app.title("App - Campeonatos")
+    frameFormCampeonato.pack_forget()
+    frameOptions.pack(padx=20, pady=40)
+    
+def cadastrarCampeonato():
+    messagebox.showinfo('Cadastro de Campeonato!')
 
+# Configurações iniciais de tela
 style = Style(theme='superhero')
 app = style.master
 
@@ -24,7 +31,7 @@ app.maxsize(1000, 800)
 frameWindow = ttk.Frame(borderwidth=2, relief='solid')
 frameTitle = ttk.Frame(frameWindow)
 frameOptions = ttk.Frame(frameWindow)
-frameForm = ttk.Frame(frameWindow)
+frameFormCampeonato = ttk.Frame(frameWindow)
 
 # Elementos do titulo exibido em tela
 titulo = ttk.Label(frameTitle, text="Scraping de Dados de Campeonatos CBF", font=('Arial', 20))
@@ -40,14 +47,22 @@ cadastroTimes.grid(row=1, column=0, pady=10)
 cadastroPartidas = ttk.Button(frameOptions, text='Cadastrar Times', width=50)
 cadastroPartidas.grid(row=2, column=0, pady=10)
 
-# Elementos do form
-labelAno = ttk.Label(frameForm, text='Escolha o ano: ', font=(None, 10))
+# Elementos do form de cadastro de campeonato
+labelAno = ttk.Label(frameFormCampeonato, text='Escolha o ano: ', font=(None, 10), width=20)
 labelAno.grid(row=1, column=0, padx=10, pady=5)
 
-comboAno = ttk.Combobox(frameForm, values=anos, font=(None, 10), width=40)
+comboAno = ttk.Combobox(frameFormCampeonato, values=anos, font=(None, 10), width=20)
 comboAno.set(2023)
 comboAno.grid(row=1, column=1, padx=10, pady=5)
 
+buttonVoltar = ttk.Button(frameFormCampeonato, text='Voltar', command=closeFormCampeonatos, width=20)
+buttonVoltar.grid(row=3, column=0, padx=10, pady=10)
+
+buttonCadastrar = ttk.Button(frameFormCampeonato, text='Cadastrar', command=cadastrarCampeonato, width=20)
+buttonCadastrar.grid(row=3, column=1, padx=10, pady=10)
+
+
+# Inicialização dos Frames principais
 frameWindow.pack(padx=20, pady=10)
 frameTitle.pack(padx=20, pady=10)
 frameOptions.pack(padx=20, pady=40)
