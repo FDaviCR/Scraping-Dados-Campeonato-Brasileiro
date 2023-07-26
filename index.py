ef84b725c9c1621dee61d35b0087b68ffbdb6007
Davi <davicamiloribeiro@gmail.com>
2023-07-26 20:28:43 -0300

    Cadastro de Times

--- index.py
+++ index.py
@@ -11,13 +11,22 @@ def openFormCampeonatos():
     frameFormCampeonato.pack(padx=20, pady=40)
     frameOptions.pack_forget()
     
-def closeFormCampeonatos():
+def openFormTimes():
+    app.title('Cadastro de Times')
+    frameFormTime.pack(padx=20, pady=40)
+    frameOptions.pack_forget()
+    
+def closeForm():
     app.title("App - Campeonatos")
     frameFormCampeonato.pack_forget()
+    frameFormTime.pack_forget()
     frameOptions.pack(padx=20, pady=40)
     
 def cadastrarCampeonato():
     messagebox.showinfo('Cadastro de Campeonato!')
+    
+def cadastrarTime():
+    messagebox.showinfo(title='Mensagem de retorno', message='Cadastro de Time')
 
 # Configurações iniciais de tela
 style = Style(theme='superhero')
@@ -32,6 +41,7 @@ frameWindow = ttk.Frame(borderwidth=2, relief='solid')
 frameTitle = ttk.Frame(frameWindow)
 frameOptions = ttk.Frame(frameWindow)
 frameFormCampeonato = ttk.Frame(frameWindow)
+frameFormTime = ttk.Frame(frameWindow)
 
 # Elementos do titulo exibido em tela
 titulo = ttk.Label(frameTitle, text="Scraping de Dados de Campeonatos CBF", font=('Arial', 20))
@@ -41,7 +51,7 @@ titulo.grid(row=0, column=0, padx=10, pady=10)
 cadastroCampeonatos = ttk.Button(frameOptions, text='Cadastrar Campeonatos', width=50, command=openFormCampeonatos)
 cadastroCampeonatos.grid(row=0, column=0, pady=10)
 
-cadastroTimes = ttk.Button(frameOptions, text='Cadastrar Times', width=50)
+cadastroTimes = ttk.Button(frameOptions, text='Cadastrar Times', width=50, command=openFormTimes)
 cadastroTimes.grid(row=1, column=0, pady=10)
 
 cadastroPartidas = ttk.Button(frameOptions, text='Cadastrar Times', width=50)
@@ -55,11 +65,28 @@ comboAno = ttk.Combobox(frameFormCampeonato, values=anos, font=(None, 10), width
 comboAno.set(2023)
 comboAno.grid(row=1, column=1, padx=10, pady=5)
 
-buttonVoltar = ttk.Button(frameFormCampeonato, text='Voltar', command=closeFormCampeonatos, width=20)
+buttonVoltar = ttk.Button(frameFormCampeonato, text='Voltar', command=closeForm, width=20)
 buttonVoltar.grid(row=3, column=0, padx=10, pady=10)
 
 buttonCadastrar = ttk.Button(frameFormCampeonato, text='Cadastrar', command=cadastrarCampeonato, width=20)
 buttonCadastrar.grid(row=3, column=1, padx=10, pady=10)
+
+# Elementos do form de cadastro de times
+labelAno = ttk.Label(frameFormTime, text='Escolha o ano: ', font=(None, 10), width=20)
+labelAno.grid(row=1, column=0, padx=10, pady=5)
+
+comboAno = ttk.Combobox(frameFormTime, values=anos, font=(None, 10), width=20)
+comboAno.set(2023)
+comboAno.grid(row=1, column=1, padx=10, pady=5)
+
+buttonVoltar = ttk.Button(frameFormTime, text='Voltar', command=closeForm, width=20)
+buttonVoltar.grid(row=3, column=0, padx=10, pady=10)
+
+buttonCadastrar = ttk.Button(frameFormTime, text='Cadastrar', command=cadastrarTime, width=20)
+buttonCadastrar.grid(row=3, column=1, padx=10, pady=10)
+
+
+# Elementos do form de cadastro e atualização de partidas
 
 
 # Inicialização dos Frames principais
